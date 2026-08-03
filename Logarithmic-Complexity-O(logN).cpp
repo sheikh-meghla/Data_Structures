f379b8281d69cb1b58898803dbc2a19288146ef9
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-03 19:32:41 +0600

    Add initial implementation of Logarithmic Complexity O(logN) program

---
+++ Logarithmic-Complexity-O(logN).cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    for(int i = 1;i <= n;i*=2){
+
+        cout << i <<" ";
+    }
+    cout << endl;
+    for (int i = n; i >= 1; i/=2)
+    {
+        cout << i << " ";
+    }
+
+    return 0;
+}
