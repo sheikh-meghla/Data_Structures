25cc13bb8bfd47ba1cfefd531849435f3719511a
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-04 21:45:31 +0600

    Add initial implementation of Vector Iterators program in C++

---
+++ Vector Iterators.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    vector<int> v = {1,2,3,4,7,3,2,3,4,6,2,100};
+  
+    for(auto it = v.begin(); it<v.end(); it++) {
+
+        cout << *it << " ";
+        
+    }
+
+
+    
+    
+    return 0;
+}
