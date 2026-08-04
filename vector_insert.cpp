a7ff339a18077eeadc9ecbdda1953fcf2a3c8cf4
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-04 21:08:32 +0600

    Update vector insertion logic to erase a range of elements instead of a single element

--- vector_insert.cpp
+++ vector_insert.cpp
@@ -9,7 +9,8 @@ int main() {
     vector<int> v2 = {1,80,120,70};
     
     // v.insert(v.begin() + 2, v2.begin(), v2.end());
-    v.erase(v.begin()+2);
+    // v.erase(v.begin()+2);
+    v.erase(v.begin()+1,v.begin()+3);
     for (int x : v)
     {
         cout<< x <<" ";
