699c73f27447586b0724cfb26d9f164ee8580464
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-04 21:05:22 +0600

    Update vector insert demonstration to reflect changes in output

--- vector_insert.cpp
+++ vector_insert.cpp
@@ -5,9 +5,11 @@ using namespace std;
 int main() {
 
     vector<int> v = {10,20,30,40};
+    // v.insert(v.begin()+2,100);
     vector<int> v2 = {1,80,120,70};
     
-    v.insert(v.begin() + 2, v2.begin(), v2.end());
+    // v.insert(v.begin() + 2, v2.begin(), v2.end());
+    v.erase(v.begin()+2);
     for (int x : v)
     {
         cout<< x <<" ";
