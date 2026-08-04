5327991fef9a848dc4358b85b3e4ee213cdf4adf
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-04 21:31:10 +0600

    Update vector initialization and replace logic in Vector-Modifiers-II program

--- Vector-Modifiers-II.cpp
+++ Vector-Modifiers-II.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main() {
 
-    vector<int> v = {1,2,3,4,7,3,2,3,4,6};
+    vector<int> v = {1,2,3,4,7,3,2,3,4,6,2};
   
-    replace(v.begin()+1,v.end(),3,300);
+    replace(v.begin()+1,v.end()-1,2,300);
     for (int x : v)
     {
         cout<< x <<" ";
