5ccb5b02351f42d0b906ee41aa09a52a96739718
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-04 21:40:19 +0600

    Add initial implementation of Vector Element Access program in C++

---
+++ Vector Element Access.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    vector<int> v = {1,2,3,4,7,3,2,3,4,6,2,100};
+  
+    cout << v[3]<<endl;
+    cout << v.back()<<endl;
+    cout << v.front()<<endl;
+
+    
+    
+    return 0;
+}
