69edc74f6d6842cb43275df368f2d56e593b0c04
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-04 21:34:02 +0600

    Update vector initialization and modify output messages in Vector-Modifiers-II program

--- Vector-Modifiers-II.cpp
+++ Vector-Modifiers-II.cpp
@@ -4,13 +4,23 @@ using namespace std;
 
 int main() {
 
-    vector<int> v = {1,2,3,4,7,3,2,3,4,6,2};
+    vector<int> v = {1,2,3,4,7,3,2,3,4,6,2,100};
   
-    replace(v.begin()+1,v.end()-1,2,300);
-    for (int x : v)
+    // replace(v.begin()+1,v.end()-1,2,300);
+    // for (int x : v)
+    // {
+    //     cout<< x <<" ";
+    // }
+
+    auto it = find(v.begin(),v.end(),100);
+    if (it == v.end())
     {
-        cout<< x <<" ";
+        cout << "not fount\n" ;
     }
+    else{
+        cout << "fount\n";
+    }
+    
     
     
     return 0;
