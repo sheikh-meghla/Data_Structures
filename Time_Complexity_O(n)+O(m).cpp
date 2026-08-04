0866a68cf217da84cc2aa1a97af225a85503ef75
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-04 18:25:23 +0600

    Add initial implementation of test.cpp

---
+++ Time_Complexity_O(n)+O(m).cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++) {
+        cout << "A ";
+    }
+
+    cout << endl;
+
+    for (int i = 0; i < M; i++) {
+        cout << "B ";
+    }
+
+    return 0;
+}
