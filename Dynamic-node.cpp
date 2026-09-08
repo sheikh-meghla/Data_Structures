4f5183569836fafdb61b068b1fca1700cc908c06
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-09-08 18:54:00 +0600

    Refactor code structure for improved readability and maintainability

--- Dynamic-node.cpp
+++ Dynamic-node.cpp
@@ -15,14 +15,16 @@ class Node {
 
 int main() {
 
-    Node a(10),b(20),c(30);
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* b = new Node(30);
 
-    a.next = &b;
-    b.next = &c;
+    head->next = a;
+    a->next = b;
 
-    cout << a.val <<endl;
-    cout << a.next ->val << endl;
-    cout << a.next ->next->val<<endl;
+    cout << head->val << endl;
+    cout << head->next->val<< endl;
+    cout << head->next->next->val << endl;
     
     
     return 0;
