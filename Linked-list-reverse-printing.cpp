a47fb71830caf9c8c168cd3f2c9ff8eec8d0e888
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-09-12 07:06:15 +0600

    Refactor code structure for improved readability and maintainability

--- Linked-list-reverse-printing.cpp
+++ Linked-list-reverse-printing.cpp
@@ -12,6 +12,40 @@ class Node {
 
 };
 
+void Insert_at_tail(Node* &head,Node* &tail, int val){
+    Node* newnode = new Node(val);
+    if (head == NULL)
+    {
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+    
+    tail->next = newnode;
+    tail = newnode;
+
+}
+
+void print_linked_list(Node* head){
+    Node* tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val<<endl;
+        tmp = tmp->next;
+    }
+    
+}
+
+void print_reverse_linked_list(Node* tmp){
+
+    if (tmp ==NULL)
+    {
+        return;
+    }
+    print_reverse_linked_list(tmp->next);
+    cout << tmp->val<<endl;
+    
+}
 
 int main() {
 
@@ -27,8 +61,11 @@ int main() {
         {
             break;
         }
+        Insert_at_tail(head,tail,val);
+
         
     }
+    print_reverse_linked_list(head);
     
    
     
